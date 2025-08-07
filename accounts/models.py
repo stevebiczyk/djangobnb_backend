4daa92ce6f38b11b2a4d2fee5dd252dfff8c65ab
stevebiczyk <stevebiczyk@gmail.com>
2025-08-07 13:38:33 +0100

    created accounts app and added user model

---
+++ accounts/models.py
@@ -0,0 +1,25 @@
+import uuid
+
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser, PermissionsMixin, UserManager
+from django.db import models
+
+class CustomUserManager(UserManager):
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_user(self, name=None, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(name, email, password, **extra_fields)
+
+    def create_superuser(self, name=None, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self._create_user(name, email, password, **extra_fields)
