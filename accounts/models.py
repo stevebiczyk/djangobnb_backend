de10e22cf3f966efcd43a066b31bbc139aec5fc0
stevebiczyk <stevebiczyk@gmail.com>
2025-08-08 01:29:49 +0100

    added new properties app

--- accounts/models.py
+++ accounts/models.py
@@ -1,18 +1,10 @@
 import uuid
 
 from django.conf import settings
-from django.contrib.auth.models import AbstractUser, PermissionsMixin, UserManager
+from django.contrib.auth.models import AbstractUser, PermissionsMixin, UserManager, AbstractBaseUser
 from django.db import models
 
 class CustomUserManager(UserManager):
-    def create_user(self, email, password, **extra_fields):
-        if not email:
-            raise ValueError("The Email field must be set")
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
     
     def create_user(self, name=None, email=None, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', False)
@@ -23,3 +15,20 @@ class CustomUserManager(UserManager):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
         return self._create_user(name, email, password, **extra_fields)
+    
+class User(AbstractBaseUser, PermissionsMixin):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255, blank=True, null=True)
+    email = models.EmailField(unique=True)
+    avatar = models.ImageField(upload_to='uploads/avatars', blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now=True)
+    
+    customObjects = CustomUserManager()
+    
+    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
--- djangobnb_backend/settings.py
+++ djangobnb_backend/settings.py
@@ -27,6 +27,8 @@ DEBUG = bool(os.environ.get('DEBUG', default='0'))
 
 ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(" ")
 
+AUTH_USER_MODEL = 'accounts.User'
+
 
 # Application definition
 
@@ -37,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'accounts',  # Custom user app
+    'properties',  # Example app for properties
 ]
 
 MIDDLEWARE = [
